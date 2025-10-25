a9879fdcda8de596db799dc3ea08917a2d5f2b73
Thomas de Grivel <thoxdg@gmail.com>
2025-10-26 01:15:53 +0200

    cvs up -r OPENBSD_7_8

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.212 2025/09/30 14:49:51 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.212.2.1 2025/10/23 21:23:34 bluhm Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -71,11 +71,11 @@ ost="OpenBSD"
 osr="7.8"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
+#define STATUS "-stable"		/* stable branch */
 #if 0
+#define STATUS ""			/* release */
 #define STATUS "-beta"			/* just before a release */
 #define STATUS "-current"		/* just after a release */
-#define STATUS "-stable"		/* stable branch */
 #endif
 
 const char ostype[] = "${ost}";
