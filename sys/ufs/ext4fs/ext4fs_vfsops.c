f590e8a1fc1a049a67715a727679ccece3f61143
Thomas de Grivel <thoxdg@gmail.com>
2026-03-20 19:28:01 +0100

    fix ro_compat check

--- sys/ufs/ext4fs/ext4fs_vfsops.c
+++ sys/ufs/ext4fs/ext4fs_vfsops.c
@@ -497,9 +497,7 @@ ext4fs_sbcheck(struct ext4fs *sble, int ronly)
 
 	tmp = letoh32(sble->sb_feature_ro_compat) &
 		~EXT4FS_FEATURE_RO_COMPAT_SUPPORTED;
-	if (!ronly && tmp &&
-	    !(letoh32(sble->sb_feature_incompat) &
-	      EXT4FS_FEATURE_INCOMPAT_RECOVER)) {
+	if (!ronly && tmp) {
 		printf("ext4fs: unsupported R/O compat features: 0x%x ",
 		    tmp);
 		PRINTF_FEATURES(tmp, ext4fs_feature_ro_compat);
